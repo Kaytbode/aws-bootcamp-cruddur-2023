4fc3c28ffc52c9fd55ecfdd59f4ad15456685744
Kayode Oluborode <bkaoluborode@gmail.com>
2023-03-02 12:00:13 +0000

    chore: instrument frontend with honeycomb

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -10,6 +10,9 @@ import ReplyForm from '../components/ReplyForm';
 // [TODO] Authenication
 import Cookies from 'js-cookie'
 
+// honeycomb 
+import { trace } from '@opentelemetry/api';
+
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState(false);
@@ -19,20 +22,39 @@ export default function HomeFeedPage() {
   const dataFetchedRef = React.useRef(false);
 
   const loadData = async () => {
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
-      const res = await fetch(backend_url, {
-        method: "GET"
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setActivities(resJson)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+    return trace
+            .getTracer('home-activities tracer')
+            .startActiveSpan(`Request: GET ${backend_url}`, async (span) => {
+
+              const traceparent = `00-${span.spanContext().traceId}-${span.spanContext().spanId}-01`;
+                try {
+                  const res = await fetch(backend_url, {
+                    method: "GET",
+                    traceparent
+                  });
+
+                  span.setAttributes({
+                    'http.method': 'GET',
+                    'http.url': backend_url,
+                    'response.status_code': 200
+                  });
+
+                  let resJson = await res.json();
+
+                  if (res.status === 200) {
+                    setActivities(resJson);
+                    span.setStatus({ code: SpanStatusCode.OK });
+                  } else {
+                    console.log(res);
+                  }
+                } catch (err) {
+                  console.log(err);
+                  span.setStatus({ code: SpanStatusCode.ERROR, message: err.message });
+                } finally {
+                  span.end();
+                }
+            });
   };
 
   const checkAuth = async () => {
