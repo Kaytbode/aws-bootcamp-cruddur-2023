94bd39ea1ddc949d69c09d3f7c2611bad463d6a7
Kayode Oluborode <bkaoluborode@gmail.com>
2023-03-16 15:26:57 +0000

    refactor: frontend display db output

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -13,17 +13,32 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
+      results = []
+
       sql = query_wrap_array("""
-      SELECT * FROM activities
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
       """)
+
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
+          json = cur.fetchone()
           results = json[0]
-          print(result)
 
       if cognito_user_id is not None:
         extra_crud = {
@@ -41,3 +56,4 @@ class HomeActivities:
 
       span.set_attribute("app.result length", len(results))
       return results
+      
